59cce84750bb2262f8af1fa81c1022baefa954ae
estibaliz <estibaliz@Lenovo-PCTXUKI>
2020-05-12 11:08:26 +0200

    Creando Javadoc

--- EjemploJavadoc/src/ejerJavadoc/Empleado.java
+++ EjemploJavadoc/src/ejerJavadoc/Empleado.java
@@ -1,20 +1,36 @@
 package ejerJavadoc;
-
+/**
+ * @version 
+ * @author estibaliz
+ *
+ */
 public class Empleado {
 	private String nombre;
 	private String apellido;
 	private double salario; 
-//Hola
-
+//Holaa
+/**
+ * 
+ * @param nombre
+ * @param apellido
+ * @param salario
+ */
 public Empleado(String nombre, String apellido, double salario) {
 	this.nombre = nombre;
 	this.apellido = apellido;
 	this.salario = salario;
 }
+/**
+ * @see Empleado
+ * @param subida
+ */
 public void subirsalario (double subida) {
 	salario=salario+subida;
 }
-
+/**
+ * 
+ * @return
+ */
 private boolean comprobar() {
 	if (nombre.equals("")) {
 		return false;
